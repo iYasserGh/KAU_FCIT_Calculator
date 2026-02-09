1a2ebcc3061b296f16191753edff70f468a8c494
Yasser <ya3sa6@gmail.com>
2026-02-09 10:54:08 +0300

    refactor: simplify component loading logic in loader.js

--- statics/js/loader.js
+++ statics/js/loader.js
@@ -2,21 +2,7 @@ function loadComponent(url, elementId) {
     fetch(url)
         .then(response => response.text())
         .then(html => {
-            const tempDoc = document.createElement('html');
-            tempDoc.innerHTML = html;
-            console.log(tempDoc);
-            const content = tempDoc.querySelector('#content').innerHTML;
-            document.getElementById(elementId).innerHTML = content;
-            
-            tempDoc.querySelectorAll('script').forEach(link => {
-                console.log(link);
-                if (link.src && !document.querySelector(`script[src="${link.src}"]`)) {
-                    const newScript = document.createElement('script');
-                    newScript.src = link.src;
-                    document.body.appendChild(newScript);
-                }
-            });
-            tempDoc.remove();
+            document.getElementById(elementId).innerHTML = html;
         })
         .catch(error => {
             console.error('Error loading component:', error);
